30e708b34c3b6105891b7a3ebeb5196f02592f4a
T K <tyler@Ts-MacBook-Pro.local>
2020-01-29 14:36:30 -0800

    Cleans up whitespace for eslint

--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import ReviewModule from './reviews/ReviewModule.jsx';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+// class App extends Component {
+//   constructor(props) {
+//       super(props);
+//   }
 
-    render() {
-        return (
-            <ReviewModule/>
-        );
-    }
-}
+//   render() {
+//       return (
+//           <ReviewModule pageId={this.props.pageId} />
+//       );
+//   }
+// }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+const App = (props) => {
+  const { pageId } = props;
+  return (
+    <ReviewModule pageId={pageId} />
+  );
+};
+
+App.propTypes = {
+  pageId: PropTypes.number.isRequired,
+};
+
+ReactDOM.render(<App pageId={1} />, document.getElementById('app'));
--- client/reviews/ReviewModule.jsx
+++ client/reviews/ReviewModule.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ReviewModule extends Component {
-    constructor(props) {
-        super(props);
-    }
+// class ReviewModule extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-    render() {
-        return (
-            <h1>hello warld</h1>
-        );
-    }
-}
+//   render() {
+//     return (
+//       <h1>hello warld</h1>
+//     );
+//   }
+// }
+
+const ReviewModule = () => (
+  <h1>hello warld</h1>
+);
 
 export default ReviewModule;
