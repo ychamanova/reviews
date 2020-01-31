1f98870681f8bad1f0d7290f0738527e0ca24602
T K <tyler@Ts-MacBook-Pro.local>
2020-01-31 09:32:33 -0800

    Pushing small change in order to branch for proper workflow

--- client/reviews/ReviewModule.jsx
+++ client/reviews/ReviewModule.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // class ReviewModule extends Component {
 //   constructor(props) {
@@ -12,8 +13,17 @@ import React from 'react';
 //   }
 // }
 
-const ReviewModule = () => (
-  <h1>hello warld</h1>
-);
+const ReviewModule = (props) => {
+  const { pageId } = props;
+  return (
+    <h1>
+      Page Id: {pageId}
+    </h1>
+  );
+};
+
+ReviewModule.propTypes = {
+  pageId: PropTypes.number.isRequired,
+};
 
 export default ReviewModule;
