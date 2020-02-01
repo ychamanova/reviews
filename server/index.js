71b6df1146f2ae18f3a05a8616e820bb6d62f46e
T K <tyler@Ts-MacBook-Pro.local>
2020-02-01 12:52:20 -0800

    Adds overview header with review count

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
 const db = require('../database/index.js');
 
 const server = express();
 const PORT = 3000;
 
+server.use(morgan(':url'));
 server.use(express.static(path.join(__dirname, '/../public')));
 
-server.get('/reviews/:page_id', (req, res) => {
-  db.getAllReviewsForPage(req.params.page_id, (err, results) => {
+server.get('/reviews/:pageId', (req, res) => {
+  console.log(req.params);
+  db.getAllReviewsForPage(req.params.pageId, (err, results) => {
     if (err) {
       console.log('error from db query for reviews', err);
       res.send(err);
